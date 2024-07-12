0b41b1fe9dfe6204f338c8f7d7f6bedd34180cbe
Nico’s hidden source codes <157768835+Joshhi1@users.noreply.github.com>
2024-07-12 21:29:51 +0800

    Update Hero.jsx

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -36,11 +36,11 @@ function Hero() {
       </div>
       <div className={styles.info}>
         <h1>
-          Harris
+          Amiel Nico
           <br />
-          Johnsen
+          Diosa (NicoDev)
         </h1>
-        <h2>Frontend Developer</h2>
+        <h2>Software Developer</h2>
         <span>
           <a href="https://twitter.com/" target="_blank">
             <img src={twitterIcon} alt="Twitter icon" />
